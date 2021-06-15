c61a26174eda156606dfdb94d249ad197149aac4
Sid-Ali-Mounib <sidali-m@hotmail.com>
2021-06-15 11:43:05 +0200

    Algorithm fixed

--- src/CardSolver.java
+++ src/CardSolver.java
@@ -14,14 +14,15 @@ public class CardSolver {
 
         for (int i = 0; i < rowCard.length; i++) {
 
+
             for (int j = 0; j < rowCard.length; j++) {
 
 
                         if (rowCard[i].size()!=0 && rowCard[j].size() != 0 ){
 
                             int movingCardFrom=rowCard[i].getCard(0).getRank().getValue();
-                            int movingCardTo=rowCard[j].getCard(0).getRank().getValue()-1;
-                            boolean cardsHaveDifferentColor = rowCard[i].getCard(0).isRed()!=rowCard[j].getCard(0).isRed();
+                            int movingCardTo=rowCard[j].getCard(rowCard[j].size()-1).getRank().getValue()-1;
+                            boolean cardsHaveDifferentColor = rowCard[i].getCard(0).isRed()!=rowCard[j].getCard(rowCard[j].size()-1).isRed();
                             boolean notOnTheSameRow = rowCard[i]!=rowCard[j];
 
 
@@ -33,10 +34,6 @@ public class CardSolver {
                                 }
                             }
                         }
-
-
-
-
 
             }
         }
